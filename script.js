1443f1851e85e0ce5c2ce304dbdc297bf257003c
Caden <84691900+cyphercrit@users.noreply.github.com>
2024-09-29 22:37:00 -0500

    Added demo field

--- script.js
+++ script.js
@@ -14,20 +14,23 @@ let board = Desmos.GraphingCalculator(elt, {
 
 // initialize mathquill and http elements
 let MQ = MathQuill.getInterface(2);
-let mathFieldSpan = document.getElementById('mathquill-input');
 let fx = document.getElementById('function-decl');
 let originalid = document.getElementById('original');
-let derivativeid = document.getElementById('derivative')
+let derivativeid = document.getElementById('derivative');
 let fxField = MQ.StaticMath(fx);
 fxField.latex('f(x)=');
 let originalLegend = MQ.StaticMath(originalid);
-originalLegend.latex('f(x)=');
+originalLegend.latex('f(x)\\space\\Longrightarrow\\space');
 let derivativeLegend = MQ.StaticMath(derivativeid);
-derivativeLegend.latex('f\'(x)=');
+derivativeLegend.latex('f\'(x)\\space\\Longrightarrow\\space');
+
+let mathFieldSpan = document.getElementById('mathquill-input');
 let mathField = MQ.MathField(mathFieldSpan, {
     spaceBehavesLikeTab: true,
     autoCommands: 'pi sqrt',
 });
+mathField.latex("\\sin^2(x)+cos(x)");
+handleGraphing();
 
 // parses input into latex code
 function parseMathInput() {
